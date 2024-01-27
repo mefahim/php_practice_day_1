94a6f4943e6cb9ef7c3089b17f5f42e007edd9e0
mefahim <fahimtf1@gmail.com>
2024-01-27 18:29:28 +0600

    Variable Done

---
+++ Variables.php
@@ -0,0 +1,16 @@
+<?php
+ $my_name = "fahim";
+
+ $my_fnd_name = "Mishu";
+
+ echo "hey! My name is ". $my_name . "My Friend name is " . $my_fnd_name;
+ echo "\n";
+//  another way to showing variable data.
+$my_full_name = "Md. Faisal Hossen Fahim";
+$my_age = 24;
+$my_fnd_full_name = "Mishu";
+$my_fnd_age = 23;
+
+// Showing data
+
+echo "My name is {$my_full_name}. My age is {$my_age}. My Friend name is {$my_fnd_full_name}. His age is {$my_fnd_age}.";
