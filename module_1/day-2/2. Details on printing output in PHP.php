c19d191e8104c9b0f6ceddce9284327bf690b869
mefahim <fahimtf1@gmail.com>
2024-01-29 17:14:37 +0600

    Printf function done

---
+++ module_1/day-2/2. Details on printing output in PHP.php
@@ -0,0 +1,20 @@
+<?php
+/*
+Type of variables.
+
+1. Intiger / int
+2. double
+3. boolen
+4. null
+5. string
+6. array
+7. object
+8. resource
+*/ 
+
+// printf demo
+
+$f_name = "Faisal";
+$l_name = "Hossen";
+
+printf("His %s name is %s %s", "full", $f_name, $l_name);
